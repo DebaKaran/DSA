d4d8e3d2526dfaee58f1cbfc273c8fc4d61ea1db
DebaKaran <karan.deba@gmail.com>
2025-08-31 02:12:19 +0530

    feat: implement removeStars using stack (O(n) time, O(n) space)
    - Added RemoveStarsFromString class
    - Used Deque to simulate stack for removing characters before '*'
    - Final string built efficiently with StringBuilder

---
+++ stacks/com/dsa/stacks/RemoveStarsFromString.java
@@ -0,0 +1,46 @@
+
+//https://leetcode.com/problems/remove-stars-from-string/description/
+//2390. Remove Stars From a String
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class RemoveStarsFromString {
+
+// 1: Time Complexity:
+
+// Iterating through all characters: O(n)
+
+// Building the result string: O(n)
+
+// Total = O(2n) → O(n) (linear time)
+
+// 2: Space Complexity:
+
+// Stack/Deque may store up to all characters: O(n)
+
+// StringBuilder result: O(n)
+
+ // Total = O(n)
+
+    public String removeStars(String s) {
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for(char ch : s.toCharArray()) {
+            if(ch == '*' && !deque.isEmpty()) {
+                deque.pop();
+            } else {
+                deque.push(ch);
+            }
+            
+        }
+
+        // Build result in correct order
+        StringBuilder sb = new StringBuilder(deque.size());
+        while(!deque.isEmpty()) {
+           sb.append(deque.removeLast()); // append in original order
+        } 
+
+        return sb.toString();
+    }
+}
