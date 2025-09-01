156fec9a122e58be93adc38582d2f13d1eb2ad99
DebaKaran <karan.deba@gmail.com>
2025-09-02 00:24:40 +0530

    feat(dp): add memoization to PerfectSubsetSum to optimize recursive solution
    - Introduced memoization using a 2D dp array [target+1][n] to cache intermediate results
    - Avoids recomputation of overlapping subproblems
    - Reduces time complexity from O(2^n) to O(n * target)
    - Space complexity is O(n * target) due to memo table

--- recursion/PerfectSumSubsequences.java
+++ recursion/PerfectSumSubsequences.java
@@ -1,11 +1,48 @@
 
 //GFG Problem: https://www.geeksforgeeks.org/problems/perfect-sum-problem5633/1
 
+import java.util.Arrays;
+
 public class PerfectSumSubsequences {
 
     // Function to calculate the number of subsets with a given sum
     public int perfectSum(int[] nums, int target) {
-        return perfectSum(nums, target, 0);
+        //return perfectSum(nums, target, 0);
+
+        int[][] memos = new int[target + 1][nums.length];
+        for(int[] memo : memos) {
+            Arrays.fill(memo, -1);
+        }
+        
+        return perfectSumUsingMemo(nums, target, 0, memos);
+        
+    }
+    
+    //Time Complexity: O(n * target)
+    //Space Complexity: O(n * target) for the memoization table + O(n)
+    
+    private int perfectSumUsingMemo(int[] nums, int target, int idx, int[][] memos) {
+         // Base case
+        if(idx >= nums.length) {
+            return target == 0 ? 1 : 0;
+        }
+        
+         // Already computed?
+        if(memos[target][idx] != -1) {
+            return memos[target][idx];
+        }
+        
+        // Option 1: Include current number (if ≤ target)
+        int included = 0;
+        if(nums[idx] <= target) {
+            included = perfectSumUsingMemo(nums, target - nums[idx], idx + 1, memos);
+        }
+        
+        // Option 2: Exclude current number
+        int excluded = perfectSumUsingMemo(nums, target, idx + 1, memos);
+        
+        // Save & return
+        return memos[target][idx] = included + excluded;
     }
     
     //Time Complexity: O(2^n)
