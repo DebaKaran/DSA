7f67215d587480a55bf25c91b93708b8315cfb12
DebaKaran <karan.deba@gmail.com>
2025-09-01 23:42:58 +0530

    sum variable is not required

--- recursion/PerfectSumSubsequences.java
+++ recursion/PerfectSumSubsequences.java
@@ -5,24 +5,24 @@ public class PerfectSumSubsequences {
 
     // Function to calculate the number of subsets with a given sum
     public int perfectSum(int[] nums, int target) {
-        return perfectSum(nums, target, 0, 0);
+        return perfectSum(nums, target, 0);
     }
     
     //Time Complexity: O(2^n)
     //Space Complexity: O(n) for the recursion stack
     //TLE for large inputs
-    
-    private int perfectSum(int[] nums, int target, int sum, int idx) {
+
+    private int perfectSum(int[] nums, int target, int idx) {
         if (idx >= nums.length) {
-            return sum == target ? 1 : 0;
+            return target == 0 ? 1 : 0;
         }
         
         int included = 0;
-        if (sum + nums[idx] <= target) {
-            included = perfectSum(nums, target, sum + nums[idx], idx + 1);
+        if (nums[idx] <= target) {
+            included = perfectSum(nums, target - nums[idx], idx + 1);
         }
         
-        int excluded = perfectSum(nums, target, sum, idx + 1);
+        int excluded = perfectSum(nums, target, idx + 1);
         
         return included + excluded;
     }
