88d91a74f2a857a3ca5aabc68155934bc9d91772
DebaKaran <karan.deba@gmail.com>
2025-09-04 01:21:03 +0530

    PostOrder traversal using two stacks

--- binarytree/PostorderTraversalBinaryTree.java
+++ binarytree/PostorderTraversalBinaryTree.java
@@ -2,6 +2,7 @@ package binarytree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class PostorderTraversalBinaryTree {
     public List<Integer> postorderTraversal(TreeNode root) {
@@ -24,4 +25,37 @@ public class PostorderTraversalBinaryTree {
         result.add(node.val);
         
     }
+
+
+    //postorder is: left ---> right ----> root
+	//preOrder is: root ---> left ----> right
+	//customise preOrder is: root ----> right  ----> left
+	//reversal of customise preOrder is: left ---> right ----> root which is same as postOrder
+	
+	private static List<Integer> postOrderUsingTwoStack(TreeNode node) {
+        List<Integer> result = new ArrayList<>();
+        if(node == null) {
+            return result;
+        }
+
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        stack1.push(node);
+        while (!stack1.isEmpty()) {
+            TreeNode current = stack1.pop();
+            stack2.push(current);
+            if(current.left != null) {
+                stack1.push(current.left);
+            }
+            if(current.right != null) {
+                stack1.push(current.right);
+            }
+            
+        }
+
+        while (!stack2.isEmpty()) {
+            result.add(stack2.pop().val);
+        }
+        return result;
+    }   
 }
