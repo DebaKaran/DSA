55885862a3b67e89e96c4b6760b0a07656fb1852
DebaKaran <karan.deba@gmail.com>
2025-09-04 00:51:49 +0530

    Iterative Approach using stack

--- binarytree/InorderTraversalBinaryTree.java
+++ binarytree/InorderTraversalBinaryTree.java
@@ -2,11 +2,35 @@ package binarytree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class InorderTraversalBinaryTree {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
+        /**List<Integer> result = new ArrayList<>();
         inorderTraversal(root, result);
+        return result;*/
+        return inorderTraversalIterative(root);
+    }
+
+    private List<Integer> inorderTraversalIterative(TreeNode node) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode curr = node;
+        while(curr != null || !stack.isEmpty()) {
+            // go left as far as possible
+            while(curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            // backtrack
+            curr = stack.pop();
+            result.add(curr.val);
+
+            // go right
+            curr = curr.right;
+        }
         return result;
     }
 
