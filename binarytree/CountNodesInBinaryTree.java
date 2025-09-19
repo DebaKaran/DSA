d1fb0e9119d91118a4aa6dd6baff1c2cfadac08f
DebaKaran <karan.deba@gmail.com>
2025-09-19 21:22:23 +0530

    nodes count

--- binarytree/CountNodesInBinaryTree.java
+++ binarytree/CountNodesInBinaryTree.java
@@ -5,28 +5,30 @@ import java.util.Queue;
 
 public class CountNodesInBinaryTree {
     public int countNodes(TreeNode root) {
-    if (root == null) return 0;  // important base case
-
-    int count = 0;
-    Queue<TreeNode> queue = new LinkedList<>();
-    queue.add(root);
-
-    while (!queue.isEmpty()) {
-        TreeNode curr = queue.remove();
-        count++;
-
-        if (curr.left != null) {
-            queue.add(curr.left);
-        }
-
-        if (curr.right != null) {
-            queue.add(curr.right);
-        }
-    }
-
-    return count;
+        return countNodesUsingBFS(root);
 }
 
+    private  int countNodesUsingBFS(TreeNode root) {
+        if (root == null) return 0;  // important base case
 
+        int count = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode curr = queue.remove();
+            count++;
+
+            if (curr.left != null) {
+                queue.add(curr.left);
+            }
+
+            if (curr.right != null) {
+                queue.add(curr.right);
+            }
+        }
+
+        return count;
+    }
 
 }
