491870deeae494ff9b5f1c45bac9e0413d492e37
Deba Karan Agrawala <karan.deba@gmail.com>
2025-11-23 20:56:25 +0530

    UpperBound - Canonical Implementation

--- binarysearch/UpperBound.java
+++ binarysearch/UpperBound.java
@@ -8,7 +8,26 @@
  */
 public class UpperBound {
     public int upperBound(int[] nums, int x) {
-        return upperBoundUsingBinarySearch(nums, x);
+        //return upperBoundUsingBinarySearch(nums, x);
+        return upperBoundCanonicalForm(nums, x);
+        
+    }
+
+    public int upperBoundCanonicalForm(int[] nums, int x) {
+        int low = 0;
+        int high = nums.length; // exclusive
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] <= x) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low; // first > x or nums.length
     }
 
     private int upperBoundUsingBinarySearch(int[] nums, int x) {
